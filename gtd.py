e8609bb83282e65330e66db955d78e37c90036c0
David Berglund <david.berglund@orebro.se>
2022-12-20 01:14:31 +0100

    further choices

--- gtd.py
+++ gtd.py
@@ -81,9 +81,33 @@ match eisenhower:
                                                 print(task)
                                                 exit()
                                             case "NO":
+                                                print("Go ahead and schedule time for developing this idea further:")
                                                 print(task)
                                                 exit()
                                     case "NO":
                                         eisenhower = input("Then you should instead document, blog or in some other way put this so that it can be referenced in the future. Here's the task again:").upper()
                                         print(task)
                                         exit()
+            case "NO":
+                eisenhower = input("Is it actionable?").upper()
+                match eisenhower:
+                    case "YES":
+                        print("Put this task in the backlog. It's important but not urgent. Don't worry too much about the details right now (if it's a multi or single step task etc)")
+                        print(task)
+                        exit()
+                    case "NO":
+                        eisenhower = input("Is it really that important or can we please trash it right now?").upper()
+                        match eisenhower:
+                            case "YES":
+                                print("Awesome! Let's trash this task:")
+                                print(task)
+                                exit()
+                            case "NO":
+                                print("This is apparently important. But it's not urgent and it's not actionable at the moment.")
+                                print("Then this task is one of two things:")
+                                print("1. A documentation task. Something that should be documented or shared in some form or just should be available for reference later. If that's the case, then add a docs/sharing/etc TODO in the backlog.")
+                                print("2. Alternatively, this needs further incubation to be useful. If so, then add an incubation TODO in the backlog.")
+                                print("3. There's no third option. Just trash this thing and focus on something important instead!")
+                                print("")
+                                print("The task at hand:")
+                                print(task)
