e584874a9d68bf3f7713cd95b369eb55fbf60e28
David Berglund <david.berglund@orebro.se>
2023-01-05 21:12:01 +0100

    use match, case not if statements

--- todoordont.py
+++ todoordont.py
@@ -11,8 +11,7 @@ def checkUserAnswer(userAnswers):
         if userAnswers.get("urgent") == True:
             print ("Delegate? Or add to backlog of less important TODO's")
             return {'important': False, 'urgent': True}
-    if userAnswers.get("important") == True:
-        if userAnswers.get("urgent") == False:
+    if userAnswers.get("important") == True and userAnswers.get("urgent") == False:
             if "actionable" not in userAnswers:
                 return "set_actionable_key"
             if userAnswers.get("actionable") == False:
@@ -67,84 +66,83 @@ def cliLoop():
                 break
             case "set_important_key":
                 answer = input("""Is it important?""").upper()
-                if answer == "Y":
-                    userAnswers['important'] = True
-                if answer == "N":
-                    userAnswers['important'] = False
+                match answer:
+                    case "Y":
+                        userAnswers['important'] = True
+                    case "N":
+                        userAnswers['important'] = False
             case "set_urgent_key":
                 answer = input("""Is it urgent?""").upper()
-                if answer == "Y":
-                    userAnswers['urgent'] = True
-                if answer == "N":
-                    userAnswers['urgent'] = False
+                match answer:
+                    case "Y":
+                        userAnswers['urgent'] = True
+                    case "N":
+                        userAnswers['urgent'] = False
             case "set_actionable_key":
                 answer = input("""Is it actionable?""").upper()
-                if answer == "N":
-                    userAnswers['actionable'] = False
-                if answer == "Y":
-                    userAnswers['actionable'] = True
+                match answer:
+                    case "N":
+                        userAnswers['actionable'] = False
+                    case "Y":
+                        userAnswers['actionable'] = True
             case "set_singleStepTask_key":
                 answer = input("""Is this a single step task?""").upper()
-                if answer == "Y":
-                    userAnswers['singleStepTask'] = True
-                if answer == "N":
-                    userAnswers['singleStepTask'] = False
+                match answer:
+                    case "Y":
+                        userAnswers['singleStepTask'] = True
+                    case "N":
+                        userAnswers['singleStepTask'] = False
             case "set_doneIn2Mins_key":
                 answer = input("""Can you complete this in 2 mins?""").upper()
-                if answer == "Y":
-                    userAnswers['doneIn2Mins'] = True
-                if answer == "N":
-                    userAnswers['doneIn2Mins'] = False
+                match answer:
+                    case "Y":
+                        userAnswers['doneIn2Mins'] = True
+                    case "N":
+                        userAnswers['doneIn2Mins'] = False
             case "set_forReference_key":
                 answer = input("""Is this for reference or a blog post or docs to be written? Perhaps some needed reading?""").upper()
-                if answer == "Y":
-                    userAnswers['forReference'] = True
-                if answer == "N":
-                    userAnswers['forReference'] = False
+                match answer:
+                    case "Y":
+                        userAnswers['forReference'] = True
+                    case "N":
+                        userAnswers['forReference'] = False
             case "set_delegate_key":
                 answer = input("""Delegate this task?""").upper()
-                if answer == "Y":
-                    userAnswers['delegate'] = True
-                if answer == "N":
-                    userAnswers['delegate'] = False
+                match answer:
+                    case "Y":
+                        userAnswers['delegate'] = True
+                    case "N":
+                        userAnswers['delegate'] = False
+            #Add effects/output below, like automatically creating an issue with git-bug, for example
             case {'important': False, 'urgent': False}:
                 print ("Action for: Trash it!")
                 break
             case {'important': False, 'urgent': True}:
                 print ("Action for: Delegate or add to backlog of less important TODO's")
-                print ("Like creating an issue with git-bug, for example")
                 break
             case {'important': True, 'urgent': False, 'actionable': False}:
                 print ("Action for: Delegate for incubation.")
-                print ("Like creating an issue with git-bug, for example")
                 break
             case {'important': True, 'urgent': False, 'actionable': True}:
                 print ("Action for: Add to backlog.")
-                print ("Like creating an issue with git-bug, for example")
                 break
             case {'important': True, 'urgent': True, 'actionable': True, 'singleStepTask': True, 'doneIn2Mins': True}:
                 print ("Action for: Do it now!")
-                print ("Like creating an issue with git-bug, for example")
                 break
             case {'important': True, 'urgent': True, 'actionable': True, 'singleStepTask': False}:
                 print ("Action for: Project. Break it down.")
-                print ("Like creating an issue with git-bug, for example")
                 break
             case {'important': True, 'urgent': True, 'actionable': True, 'singleStepTask': True, 'doneIn2Mins': False}:
                 print ("Action for: Add to TODO ASAP.")
-                print ("Like creating an issue with git-bug, for example")
                 break
             case {'important': True, 'urgent': True, 'actionable': False, 'incubate': True, 'forReference': True}:
                 print ("Action for: Put this where it can be referenced or produce the docs/blog post etc. Right now or add to TODO ASAP.")
-                print ("Like creating an issue with git-bug, for example")
                 break
             case {'important': True, 'urgent': True, 'actionable': False, 'incubate': True, 'forReference': False, 'delegate': True}:
                 print ("Action for: Delegate for incubation.")
-                print ("Like creating an issue with git-bug, for example")
                 break
             case {'important': True, 'urgent': True, 'actionable': False, 'incubate': True, 'forReference': False, 'delegate': False}:
                 print ("Action for: Incubate now or add to TODO for incubation.")
-                print ("Like creating an issue with git-bug, for example")
                 break
 def apiLoop():
     """Run with user input over web API"""
